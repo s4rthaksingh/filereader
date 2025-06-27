f002ba99d547d14e381607beda8d536382560661
Sarthak <sarthakss898@gmail.com>
2025-06-27 11:56:15 +0530

    Callback hell

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const fs = require("fs");
+import fs from "fs";
 
-fs.readdir("./filefolder",(err,files)=>{
-    if(err) return console.log(err);
-    else{
-        fs.readFile("./filefolder/" + files[0],{encoding:"utf-8"},(err,text)=>{
-            if(err) return console.log(err);
-            console.log(text);
-        });
+async function runcode() {
+  fs.readdir("./filefolder", (err, files) => {
+    if (err) return console.log(err);
+    else {
+      fs.readFile(
+        "./filefolder/" + files[0],
+        { encoding: "utf-8" },
+        (err, text) => {
+          if (err) return console.log(err);
+          console.log(text);
+          fs.writeFile(
+            "./filefolder/file to write",
+            "Hey im jsust writing into this file the 2nd time, dont mind me...",
+            (err) => {
+              if (err) console.log(err);
+              else
+                fs.readdir("./filefolder", (err, files) => {
+                  if (err) return console.log(err);
+                  else {
+                    fs.readFile(
+                      "./filefolder/" + files[0],
+                      { encoding: "utf-8" },
+                      (err, text) => {
+                        if (err) return console.log(err);
+                        console.log(text);
+                      }
+                    );
+                  }
+                });
+            }
+          );
+        }
+      );
     }
-})
+  });
+}
+
+runcode();
